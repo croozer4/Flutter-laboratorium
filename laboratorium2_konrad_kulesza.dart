efe6c345f7fee62fb7b56f3b89502a289dfb746a
croozer4 <94395053+croozer4@users.noreply.github.com>
2023-03-17 11:37:54 +0100

    Add files via upload

--- laboratorium2_konrad_kulesza.dart
+++ laboratorium2_konrad_kulesza.dart
@@ -1,10 +1,68 @@
+import 'dart:convert';
+
+List<Pojazd> listaWszystkichPojazdow = [];
+
 void main() {
+  String pojazdy = '''<pojazdy>
+    <samochod>
+        <nazwa>Honda Civic</nazwa>
+        <rodzajPaliwa>3</rodzajPaliwa>
+    </samochod>
+    <motorowka>
+        <nazwa>Super motorowka</nazwa>
+        <rodzajPaliwa>0</rodzajPaliwa>
+    </motorowka>
+    <samochod>
+        <nazwa>Mercedes CLK</nazwa>
+        <rodzajPaliwa>5</rodzajPaliwa>
+    </samochod>
+    <rower>
+        <nazwa>Giant</nazwa>
+    </rower>
+    <hulajnoga>
+        <nazwa>Najfajniejsza hulajnoga</nazwa>
+    </hulajnoga>
+</pojazdy>''';
   
+    var xmlDoc = XmlDocument.parse(pojazdy);
+  var pojazdyList = xmlDoc.findAllElements('pojazdy').expand((p) => p.children);
+  var wypozyczalnia = Wypozyczalnia();
+
+  pojazdyList.forEach((pojazd) {
+    var nazwa = pojazd.findElements('nazwa').single.text;
+
+    if (pojazd.tagName == 'samochod') {
+      var rodzajPaliwa = int.parse(pojazd.findElements('rodzajPaliwa').single.text);
+      var samochod = Samochod(nazwa, rodzajPaliwa);
+      wypozyczalnia.dodajPojazd(samochod);
+    } else if (pojazd.tagName == 'motorowka') {
+      var rodzajPaliwa = int.parse(pojazd.findElements('rodzajPaliwa').single.text);
+      var motorowka = Motorowka(nazwa, rodzajPaliwa);
+      wypozyczalnia.dodajPojazd(motorowka);
+    } else if (pojazd.tagName == 'rower') {
+      var rower = Rower(nazwa);
+      wypozyczalnia.dodajPojazd(rower);
+    } else if (pojazd.tagName == 'hulajnoga') {
+      var hulajnoga = Hulajnoga(nazwa);
+      wypozyczalnia.dodajPojazd(hulajnoga);
+    }
+  });
+
+  print(wypozyczalnia);
+  
+//   Samochod samochod1 = new Samochod("Renault", "Benzyna");
+//   print(samochod1.id);
+  
+//   Samochod samochod2 = new Samochod("Toyota", "Diesel");
+//   print(samochod2.id);
+  
+//   Samochod samochod3 = new Samochod("Volvo", "Diesel");
+//   print(samochod3.id);
 }
 
 class Wypozyczalnia{
-  List garaze = [];
-  List pojazdy = [];
+  List<Garaz> _garaze;
+  List<Pojazd> _pojazdy = [];
   
   void usunPojazd(int idDoUsuniecia){
     pojazdy.removeWhere((element) => element['id'] == idDoUsuniecia);
@@ -17,8 +75,7 @@ class Wypozyczalnia{
   void wypiszWszystkiePojazdy(){
     print(pojazdy);
   }
-  
-  
+   
 }
 
 class Garaz{
@@ -27,6 +84,8 @@ class Garaz{
   
 }
 
+// klasy abstrakcyjne
+
 abstract class Pojazd{
   late int id;
   late String nazwaPojazdu;
@@ -34,19 +93,20 @@ abstract class Pojazd{
   Pojazd(String nazwaPojazdu){
     this.nazwaPojazdu = nazwaPojazdu;
     
-    int maxId = 0;
+    int dlugosc = listaWszystkichPojazdow.length;
     
-    listaWszystkichPojazdow.forEach( (pojazd){
-      if(pojazd.id > maxId){
-        maxId = pojazd.id;
-      }
-    });
+    if(dlugosc == 0){
+      id = 0;
+    }
+    else{
+     id = listaWszystkichPojazdow[dlugosc - 1].id + 1; 
+    } 
     
-    id = maxId + 1; 
+    listaWszystkichPojazdow.add(this);
   }
 }
 
-abstract class Samochod extends Pojazd implements Spalinowy, Parkowalny{
+class Samochod extends Pojazd implements Spalinowy, Parkowalny{
   
   late String _rodzajPaliwa;
   
@@ -56,35 +116,44 @@ abstract class Samochod extends Pojazd implements Spalinowy, Parkowalny{
   late Garaz aktualnyGaraz;
   int iloscPaliwa = 0;
   
-  Samochod(String nazwaPojazdu, this.rodzajPaliwa)
+  Samochod(String nazwaPojazdu, rodzajPaliwa)
     :super(nazwaPojazdu);
+  
+  void tankowanie(int litry, String rodzajPaliwa){
+    if(this.rodzajPaliwa == rodzajPaliwa){
+      print("Zatankowano pojazd");
+    }
+    else{
+      print("To nie jest właściwy rodzaj paliwa");
+    }
+  }
+  
+  void jakiePaliwo(){
+    print("Pojazd korzysta z paliwa: $rodzajPaliwa");
+  }
+  
+  void zaparkujPojazd(Garaz wybranyGaraz){
+    if(aktualnyGaraz.czyPusty){
+      aktualnyGaraz = wybranyGaraz;
+      wybranyGaraz.aktualnyPojazd = this;
+      print("Pojazd został zaparkowany");
+    }
+    print("Pojazd nie został zaparkowany, garaż nie jest pusty");
+  }
+  
+  void wyparkujPojazd(){
+    print("Samochod został wyparkowany");
+  }
   
 }
 
-abstract class Motorowka extends Pojazd implements Spalinowy{
+class Motorowka extends Pojazd implements Spalinowy{
   
   late String rodzajPaliwa;
   int iloscPaliwa = 0;
   
-  Motorowka(String nazwaPojazdu, this.rodzajPaliwa)
+  Motorowka(String nazwaPojazdu, rodzajPaliwa)
     :super(nazwaPojazdu);
-}
-
-abstract class Rower extends Pojazd implements Parkowalny{
-  late Garaz aktualnyGaraz;
-  
-  Rower(String nazwaPojazdu, this.aktualnyGaraz)
-    :super(nazwaPojazdu);
-}
-
-abstract class Hulajnoga extends Pojazd{
-  Hulajnoga(String nazwaPojazdu)
-    :super(nazwaPojazdu);
-}
-
-class Spalinowy{
-  String get rodzajPaliwa;
-  set rodzajPaliwa(String value);
   
   void tankowanie(int litry, String rodzajPaliwa){
     if(this.rodzajPaliwa == rodzajPaliwa){
@@ -100,10 +169,16 @@ class Spalinowy{
   }
 }
 
-class Parkowalny{
+class Rower extends Pojazd implements Parkowalny{
+  late Garaz aktualnyGaraz;
+  
+  Rower(String nazwaPojazdu, this.aktualnyGaraz)
+    :super(nazwaPojazdu);
+  
   void zaparkujPojazd(Garaz wybranyGaraz){
     if(aktualnyGaraz.czyPusty){
       aktualnyGaraz = wybranyGaraz;
+      wybranyGaraz.aktualnyPojazd = this;
       print("Pojazd został zaparkowany");
     }
     print("Pojazd nie został zaparkowany, garaż nie jest pusty");
@@ -112,4 +187,26 @@ class Parkowalny{
   void wyparkujPojazd(){
     print("Samochod został wyparkowany");
   }
+}
+
+class Hulajnoga extends Pojazd{
+  Hulajnoga(String nazwaPojazdu)
+    :super(nazwaPojazdu);
+}
+
+// interfejsy
+
+abstract class Spalinowy{
+  String get rodzajPaliwa;
+  set rodzajPaliwa(String value);
+  
+  void tankowanie(int litry, String rodzajPaliwa);
+  
+  void jakiePaliwo();
+}
+
+abstract class Parkowalny{
+  void zaparkujPojazd(Garaz wybranyGaraz);
+  
+  void wyparkujPojazd();
 }
